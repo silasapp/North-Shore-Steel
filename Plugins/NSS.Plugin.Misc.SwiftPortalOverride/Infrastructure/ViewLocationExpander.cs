0f46ca098bf2fc7137d390e501fdfe6499641a08
Chika Okonkwo <c.o@chaione.com>
2020-09-21 11:45:42 +0100

    create swift portal override plugin

---
+++ Plugins/NSS.Plugin.Misc.SwiftPortalOverride/Infrastructure/ViewLocationExpander.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NSS.Plugin.Misc.SwiftPortalOverride.Infrastructure
+{
+    public class ViewLocationExpander : IViewLocationExpander
+    {
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+        }
+
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            if (context.AreaName == "Admin")
+            {
+                viewLocations = new[] { $"/Plugins/NSS.Plugin.Misc.SwiftPortalOverride/Areas/Admin/Views/{context.ControllerName}/{context.ViewName}.cshtml" }.Concat(viewLocations);
+            }
+            else
+            {
+                viewLocations = new[] { $"/Plugins/NSS.Plugin.Misc.SwiftPortalOverride/Views/{context.ControllerName}/{context.ViewName}.cshtml" }.Concat(viewLocations);
+            }
+
+            return viewLocations;
+        }
+    }
+}
---
+++ Plugins/NSS.Plugin.Misc.SwiftPortalOverride/Mapping/Builders/PluginBuilder.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator.Builders.Create.Table;
+using Nop.Data.Mapping.Builders;
+using NSS.Plugin.Misc.SwiftPortalOverride.Domains;
+
+namespace NSS.Plugin.Misc.SwiftPortalOverride.Mapping.Builders
+{
+    public class PluginBuilder : NopEntityBuilder<CustomTable>
+    {
+        #region Methods
+
+        public override void MapEntity(CreateTableExpressionBuilder table)
+        {
+        }
+
+        #endregion
+    }
+}
