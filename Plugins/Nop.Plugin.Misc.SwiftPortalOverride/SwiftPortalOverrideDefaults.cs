bcd67a96c135129bda51670da28a70e6348f9dc6
Chika Okonkwo <c.o@chaione.com>
2020-09-24 14:00:37 +0100

    update field label and variables

--- Plugins/Nop.Plugin.Misc.SwiftPortalOverride/SwiftPortalOverrideDefaults.cs
+++ Plugins/Nop.Plugin.Misc.SwiftPortalOverride/SwiftPortalOverrideDefaults.cs
@@ -18,7 +18,7 @@ namespace Nop.Plugin.Misc.SwiftPortalOverride
 
         #region NSSCustomerAttributes           
         public static string IsExistingCustomerAttribute => "IsExistingCustomer";        
-        public static string PreferredLocationIdAttribute => "PreferredLocationId";        
+        public static string PreferredLocationIdAttribute => "PreferredLocation";        
         public static string HearAboutUsAttribute => "HearAboutUs";        
         public static string OtherAttribute => "Other";        
         public static string ItemsForNextProjectAttribute => "ItemsForNextProject"; 
