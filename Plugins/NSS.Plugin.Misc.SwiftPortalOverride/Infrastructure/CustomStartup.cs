dddd055a7e5b274b2ad0d6b3740d5e64eb3925da
Babafemi Adojutelegan <b.a@chaione.com>
2020-10-13 18:53:49 +0100

    update screen to override header navigation

---
+++ Plugins/NSS.Plugin.Misc.SwiftPortalOverride/Infrastructure/CustomStartup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nop.Core.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NSS.Plugin.Misc.SwiftPortalOverride.Infrastructure
+{
+    public class CustomStartup : INopStartup
+    {
+        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.ViewLocationExpanders.Add(new CustomViewEngine());
+            });
+        }
+
+        public void Configure(IApplicationBuilder application)
+        {
+        }
+
+        public int Order
+        {
+            get { return 1001; }
+        }
+    }
+}
--- Plugins/NSS.Plugin.Misc.SwiftPortalOverride/Infrastructure/ViewLocationExpander.cs
+++ Plugins/NSS.Plugin.Misc.SwiftPortalOverride/Infrastructure/ViewLocationExpander.cs
@@ -20,16 +20,16 @@ namespace NSS.Plugin.Misc.SwiftPortalOverride.Infrastructure
             {
                 viewLocations = new[] { $"/Plugins/NSS.Plugin.Misc.SwiftPortalOverride/Views/{context.ControllerName}/{context.ViewName}.cshtml" }.Concat(viewLocations);
 
-                if (context.Values.TryGetValue("SwiftPortal", out string theme))
-                {
-                    if (context.ViewName == "Components/TopMenu/Default")
-                    {
-                        viewLocations = new[] {
-                            $"~/Plugins/Misc.PluginName/Themes/{theme}/Views/Shared/Components/CustomTopMenu/Default.cshtml",
-                        }
-                        .Concat(viewLocations);
-                    }
-                }
+                //if (context.Values.TryGetValue("SwiftPortal", out string theme))
+                //{
+                //    if (context.ViewName == "Components/TopMenu/Default")
+                //    {
+                //        viewLocations = new[] {
+                //            $"~/Plugins/Misc.PluginName/Themes/{theme}/Views/Shared/Components/CustomTopMenu/Default.cshtml",
+                //        }
+                //        .Concat(viewLocations);
+                //    }
+                //}
             }
 
             return viewLocations;
