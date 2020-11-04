862df73d2b4c9eff5cee56dd7ed2b2f9f497a189
Babafemi Adojutelegan <b.a@chaione.com>
2020-11-04 13:21:47 +0100

    create customer company products table

---
+++ Plugins/NSS.Plugin.Misc.SwiftCore/Services/ICustomerCompanyProductService.cs
@@ -0,0 +1,15 @@
+﻿using NSS.Plugin.Misc.SwiftCore.Domain.Customers;
+using System.Collections.Generic;
+
+namespace NSS.Plugin.Misc.SwiftCore.Services
+{
+    public interface ICustomerCompanyProductsService
+    {
+        void InsertCustomerCompanyProducts(List<CustomerCompanyProduct> customerCompanyProducts);
+
+        IList<CustomerCompanyProduct> GetCustomerCompanyProducts();
+
+        CustomerCompanyProduct GetCustomerCompanyProductById(int customerId, int companyId, int productId);
+
+    }
+}
