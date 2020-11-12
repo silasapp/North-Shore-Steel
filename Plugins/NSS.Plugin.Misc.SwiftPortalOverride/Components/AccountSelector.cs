e4b99a7946cb287562bf534d83ec93f7452c6d92
Ayoola Akindolani <a.a@chaione.com>
2020-11-12 04:19:29 +0100

    work on account selection

---
+++ Plugins/NSS.Plugin.Misc.SwiftPortalOverride/Components/AccountSelector.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Factories;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Web.Components
+{
+    public class AccountSelectorViewComponent : NopViewComponent
+    {
+        private readonly ICatalogModelFactory _catalogModelFactory;
+
+        public AccountSelectorViewComponent(ICatalogModelFactory catalogModelFactory)
+        {
+            _catalogModelFactory = catalogModelFactory;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View("~/Plugins/Misc.SwiftPortalOverride/Views/Shared/Components/AccountSelector/Default.cshtml");
+        }
+    }
+}
