bdc8f7eccdce4e67ded3d29bfcc6c211dc3b89bd
Roberto Aballe <roberto.aballe@chaione.com>
2021-01-19 13:08:46 -0600

    Updating lablel from "Sub-Total" to "SubTotal"

---
+++ Plugins/NSS.Plugin.Misc.SwiftApi/Data/UpdateOrderSubtotalLocaleStringResourceValue.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+using Nop.Data.Migrations;
+
+namespace NSS.Plugin.Misc.SwiftApi.Data
+{
+    [NopMigration("2021/01/19 10:36:00", "Swift.Api Update Order.Subtotal Value in LocaleStringResource")]
+    public class UpdateOrderSubtotalLocaleStringResourceValue : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql("Update [LocaleStringResource] Set [ResourceValue] = 'SubTotal' Where [ResourceName] = 'Order.SubTotal'");
+        }
+
+        public override void Down()
+        {
+            Execute.Sql("Update [LocaleStringResource] Set [ResourceValue] = 'Sub-Total' Where [ResourceName] = 'Order.SubTotal'");
+        }
+    }
+}
